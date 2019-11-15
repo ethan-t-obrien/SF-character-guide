6c7729fda36f635f990c561ffa39d88b18e3eb00
Rautini Thompson <Rautini Thompson>
2019-11-16 10:57:15 +1300

    start on internal db

--- migrations/20191116095119_sf.js
+++ migrations/20191116095119_sf.js
@@ -1,14 +1,22 @@
 
 exports.up = (knex, Promise) => {
-    return knex.schema.createTable('sf', function (table) {
+    return knex.schema.createTable('sfCharacters', function (table) {
         table.increments().primary()
-        table.string('name')
-        table.string('profilePic')
-        
+        table.string('characterName')
+        table.string('pP')
+        table.string('origin')
+        table.string('bloodType')
+        table.integer('height')
+        table.integer('weight')
+        table.string('likes')
+        table.string('dislikes')
+        table.string('fightingStyle')
+
     })
   
-};
+};  
 
-exports.down = function(knex) {
+exports.down = (knex, Promise) => {
+    return knex.schema.dropTable('sfCharacters')
   
 };
--- seeds/sf-data.js
+++ seeds/sf-data.js
@@ -1,13 +1,13 @@
 
-exports.seed = function(knex) {
+exports.seed = (knex, Promise) => {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('sfCharacters').del()
     .then(function () {
       // Inserts seed entries
       return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+        {id: 1, characterName: 'Akuma', pP: 'qq', origin: 'Unkown (presumably Japan)', bloodType: 'Uknown', height: 178, weight: 102, likes: 'Martial Arts, Training, Raging Demon, Satsui no Hadou, Fighting Evil Ryu, Strong Opponents, Becoming stronger', dislikes: 'Gouken, Anyone who interferes with his training, Goutetsu', fightingStyle: 'Satsui no Hadou-infused ansatsuken art'},
+        {id: 2, characterName: 'Alex', pP: 'qq', origin: 'test', bloodType: 'test', height: 0, weight: 0, likes: 'test', dislikes: 'test', fightingStyle: 'test'},
+        {id: 3, characterName: 'Dudley', pP: 'qq', origin: '', bloodType: 'test', height: 0, weight: 0, likes: 'test', dislikes: 'test', fightingStyle: 'test'}
       ]);
     });
 };
